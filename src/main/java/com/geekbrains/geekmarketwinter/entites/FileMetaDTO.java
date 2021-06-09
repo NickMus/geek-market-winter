44ac0b2dec8622b11b988c92c93690a7e11ad616
nmusi <n.musinov@gmail.com>
2021-06-09 16:46:27 +0300

    hw8

--- src/main/java/com/geekbrains/geekmarketwinter/entites/FileMetaDTO.java
+++ src/main/java/com/geekbrains/geekmarketwinter/entites/FileMetaDTO.java
@@ -1,11 +1,29 @@
 package com.geekbrains.geekmarketwinter.entites;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class FileMetaDTO {
     private UUID hash;
-
     private String fileName;
+    private String date;
+    private long size;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
 
     public FileMetaDTO(String fileName) {
         this.fileName = fileName;
--- src/main/java/com/geekbrains/geekmarketwinter/repositories/interfaces/IFileMetaProvider.java
+++ src/main/java/com/geekbrains/geekmarketwinter/repositories/interfaces/IFileMetaProvider.java
@@ -2,13 +2,15 @@ package com.geekbrains.geekmarketwinter.repositories.interfaces;
 
 import com.geekbrains.geekmarketwinter.entites.FileMetaDTO;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.UUID;
 
 public interface IFileMetaProvider {
     String checkFileExists(UUID fileHash);
 
-    void saveFileMeta(UUID Hash, String fileName, int subType);
+    void saveFileMeta(UUID Hash, String fileName, int subType, String format, long size);
 
     Collection<FileMetaDTO> getMetaFiles(int subType);
 }
